ec1f0a141888098e58be10873d87bae5dbdc8f02
Vladislav <57865785+oO777Oo@users.noreply.github.com>
2020-05-25 20:33:39 +0200

    Update Main.java
    called db initialization method

--- path/Main.java
+++ path/Main.java
@@ -3,6 +3,11 @@ package bank.path;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException{
+        try {
+            Bank.dbInitialization();
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
         Bank.db.testConnection();
     }
 }
